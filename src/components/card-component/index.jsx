c10c3e285c85bdb02805a2c09d94ed9cb421c2e9
Nuri Demir <nuridemir34@hotmail.com>
2023-08-19 22:11:33 +0300

    add seacrh bar & deploy

--- src/components/card-component/index.jsx
+++ src/components/card-component/index.jsx
@@ -3,33 +3,15 @@ import ReactPaginate from "react-paginate"
 import { fetchAllData } from '~/utils/fetch'
 import CardsLoadingComponent from '../cards-loading-component'
 import CardItemComponent from '../card-item-component'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { BsArrowRight } from "react-icons/bs"
 import SearchInputComponent from '../search-input-component'
 
 export default function CardComponent(props) {
 
+    // Data fetch
     const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(false)
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
-    const handleSearch = (e) => {
-        const searchTerm = e.target.value;
-        setSearchTerm(searchTerm);
-
-        // Veri içinde arama yapın ve sonuçları ayarlayın
-        const filteredResults = data.filter((item) =>
-            item.title.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-
-        setSearchResults(filteredResults);
-    };
-
-    if (data.length < 0) {
-        return props.errorMessage
-    }
-
-
     const getAllData = async () => {
         setIsLoading(true)
         try {
@@ -41,11 +23,33 @@ export default function CardComponent(props) {
             setIsLoading(false)
         }
     }
-
     useEffect(() => {
         getAllData()
     }, [])
+    // **Data fetch
 
+    // Seacrh input
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const searchTerm = e.target.value;
+        setSearchTerm(searchTerm);
+        const filteredResults = data?.filter((item) =>
+            item.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setSearchResults(filteredResults);
+    };
+    // **Seacrh input
+
+
+    // error message
+    if (data.length < 0) {
+        return props.errorMessage
+    }
+    // **error message
+
+    // Pagination true ise
     if (props.paginate) {
         function Items({ currentItems }) {
             return (
@@ -57,7 +61,7 @@ export default function CardComponent(props) {
                                     !currentItems ? props.errorMessage : (
                                         <div className='rounded-lg'>
                                             <h2 className="text-slate-800 dark:text-slate-400 text-4xl font-bold underline-offset-8 font-playfair">{props.title}</h2>
-                                            {props.searchInput && <SearchInputComponent />}
+                                            {props.searchInput && <SearchInputComponent setSearchTerm={setSearchTerm} searchTerm={searchTerm} setSearchResults={setSearchResults} searchResults={searchResults} handleSearch={handleSearch} />}
                                             <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:px-0">
                                                 {currentItems?.map((item, index) => (
                                                     <CardItemComponent key={index} data={item} />
@@ -79,14 +83,10 @@ export default function CardComponent(props) {
 
             const endOffset = itemOffset + itemsPerPage;
             // console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-            const currentItems = data?.slice(itemOffset, endOffset);
-            const pageCount = Math.ceil(data?.length / itemsPerPage);
-
+            const currentItems = searchResults.length > 0 ? searchResults.slice(itemOffset, endOffset) : data.slice(itemOffset, endOffset);
+            const pageCount = Math.ceil((searchResults.length > 0 ? searchResults.length : data.length) / itemsPerPage);
             const handlePageClick = (event) => {
-                const newOffset = (event.selected * itemsPerPage) % data?.length;
-                // console.log(
-                //     `User requested page number ${event.selected}, which is offset ${newOffset}`
-                // );
+                const newOffset = (event.selected * itemsPerPage) % (searchResults.length > 0 ? searchResults.length : data.length);
                 setItemOffset(newOffset);
             };
 
@@ -112,17 +112,16 @@ export default function CardComponent(props) {
             </>
         )
     }
-
+    // **Pagination true ise
     return (
         <div>
             <div className='rounded-lg'>
                 {!props.href ? <h2 className=" text-slate-800 dark:text-slate-400 text-4xl font-bold underline-offset-8 font-playfair">{props.title}</h2> : (
                     <div className='flex items-center justify-between'>
                         <h2 className=" text-slate-800 dark:text-slate-400 text-4xl font-bold underline-offset-8 font-playfair">{props.title}</h2>
-                        <NavLink to={props.href} className='flex tracking-widest items-center gap-2 dark:text-blue-400 dark:hover:text-blue-500 text-blue-700 hover:text-blue-400 duration-200 font-semibold text-xl'>see more <BsArrowRight /></NavLink>
+                        <Link to={props.href} className='flex tracking-widest items-center gap-2 dark:text-blue-400 dark:hover:text-blue-500 text-blue-700 hover:text-blue-400 duration-200 font-semibold text-xl'>see more <BsArrowRight /></Link>
                     </div>
                 )}
-                {props.searchInput && <SearchInputComponent setSearchTerm={setSearchTerm} searchTerm={searchTerm} setSearchResults={setSearchResults} searchResults={searchResults} handleSearch={handleSearch} />}
                 <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:px-0 mt-8">
                     {
                         props.listItemCount ? (
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,15 +4,14 @@ import HomePage from '~/pages/home-page'
 import NotFound404Page from '~/pages/not-found-404-page'
 import PraticesPage from '~/pages/pratices-page'
 import ProjectsPage from '~/pages/projects-page'
-
 export default function RouteSystem() {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/pratices" element={<PraticesPage />} />
-            <Route path="/blogs" element={<BlogsPage />} />
-            <Route path="*" element={<NotFound404Page />} />
+            <Route path="/" exact element={<HomePage />} />
+            <Route path="/projects" exact element={<ProjectsPage />} />
+            <Route path="/pratices" exact element={<PraticesPage />} />
+            <Route path="/blogs" exact element={<BlogsPage />} />
+            <Route path="*" exact element={<NotFound404Page />} />
         </Routes>
     )
 }
