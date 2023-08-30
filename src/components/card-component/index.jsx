a1a27505af845c7848449d4c443b7cb7886b541b
Nuri Demir <nuridemir34@hotmail.com>
2023-08-30 13:11:36 +0300

    card loading finish

--- src/components/card-component/index.jsx
+++ src/components/card-component/index.jsx
@@ -3,6 +3,7 @@ import { fetchAllData } from '~/utils/fetch'
 import CardItemComponent from '../card-item-component'
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from "react-icons/bs"
+import CardsLoadingComponent from '../cards-loading-component'
 
 export default function CardComponent(props) {
 
@@ -42,15 +43,16 @@ export default function CardComponent(props) {
                         <Link to={props.href} className='flex tracking-widest items-center gap-2 dark:text-blue-400 dark:hover:text-blue-500 text-blue-700 hover:text-blue-400 duration-200 font-semibold text-xl'>see more <BsArrowRight /></Link>
                     </div>
                 )}
-                <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:px-0 mt-8">
-                    {
-                        <>
+
+                {
+                    !isLoading ? <CardsLoadingComponent /> : (
+                        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:px-0 mt-8">
                             {data?.slice(0, propsListItemCount).map((item, index) => (
                                 <CardItemComponent key={index} data={item} />
                             ))}
-                        </>
-                    }
-                </div>
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
